287a4f2ca48d0284e393450a2bee3f19823f3e06
J.neto <netikis@gmail.com>
2026-07-01 09:48:33 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'baytigrill-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './favicon.ico',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS).catch(() => {})).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  const sempreRede = url.pathname.endsWith('.html') || url.pathname.endsWith('firebase-env.js');
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200 && !sempreRede) {
+          const clone = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
